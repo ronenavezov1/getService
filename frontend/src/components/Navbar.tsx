cafb3cf04b72376c0e017fb601b7f92558ce56ce
Leon Trestman <tleonleont@gmail.com>
2023-05-01 21:44:26 +0300

    -fixed  bug by switiching into popover from menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { type FC, Fragment, type MouseEventHandler } from "react";
 import { UserRole } from "./Auth";
 import Link from "next/link";
 import { Bars3Icon } from "@heroicons/react/20/solid";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, Popover, Transition } from "@headlessui/react";
 
 interface NavbarProps {
   firstName: string;
@@ -47,17 +47,17 @@ const RightNavBar: FC<RoleProps> = ({ role }) => {
   return (
     <>
       {/* Desktop menu */}
-      <Menu as="div" className="hidden grow  justify-end md:flex">
-        <Menu.Items className="flex w-full max-w-md gap-4 " static>
+      <Popover as="div" className="hidden grow  justify-end md:flex">
+        <Popover.Panel className="flex w-full max-w-md gap-4 " static>
           <Links role={role} />
-        </Menu.Items>
-      </Menu>
+        </Popover.Panel>
+      </Popover>
 
       {/* mobile menu */}
-      <Menu as="div" className="text-center text-white md:hidden ">
+      <Popover as="div" className="text-center text-white md:hidden ">
         {({ close }) => (
           <>
-            <Menu.Button
+            <Popover.Button
               className={`justify-center focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 md:max-w-xl `}
             >
               {({ open }) => (
@@ -67,7 +67,7 @@ const RightNavBar: FC<RoleProps> = ({ role }) => {
                   }h-8 w-8 fill-yellow-400 `}
                 />
               )}
-            </Menu.Button>
+            </Popover.Button>
 
             <Transition
               as={Fragment}
@@ -78,13 +78,13 @@ const RightNavBar: FC<RoleProps> = ({ role }) => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="absolute left-0 z-10 w-full  rounded-b-md bg-indigo-600 focus:outline-none  ">
+              <Popover.Panel className="absolute left-0 z-10 w-full  rounded-b-md bg-indigo-600 focus:outline-none  ">
                 <Links role={role} closeBurger={close} />
-              </Menu.Items>
+              </Popover.Panel>
             </Transition>
           </>
         )}
-      </Menu>
+      </Popover>
     </>
   );
 };
@@ -98,22 +98,22 @@ const Links = ({ role, closeBurger: closePop }: LinksProps) => {
     <>
       {/* user links  */}
 
-      <CallsMenuItems closeBurger={closePop} />
+      <CallsMenu closeBurger={closePop} />
 
       {/* worker/admin links  */}
       {(role === UserRole.ADMIN || role === UserRole.WORKER) && (
-        <WorkMenuItems closeBurger={closePop} />
+        <WorkMenu closeBurger={closePop} />
       )}
 
       {/* admin links  */}
 
       {/* logout Button */}
-      <LogoutBtnMenuItem />
+      <LogoutBtn />
     </>
   );
 };
 
-const LogoutBtnMenuItem: FC = () => {
+const LogoutBtn: FC = () => {
   const queryClient = useQueryClient();
 
   const onClickHandler = async () => {
@@ -122,14 +122,12 @@ const LogoutBtnMenuItem: FC = () => {
   };
 
   return (
-    <Menu.Item>
-      <button
-        onClick={onClickHandler}
-        className="w-full rounded-lg bg-yellow-400  p-1 font-bold text-slate-700 hover:bg-yellow-500"
-      >
-        Logout
-      </button>
-    </Menu.Item>
+    <button
+      onClick={onClickHandler}
+      className="w-full rounded-lg bg-yellow-400  p-1 font-bold text-slate-700 hover:bg-yellow-500"
+    >
+      Logout
+    </button>
   );
 };
 
@@ -139,65 +137,13 @@ interface PopMenuProps {
   closeBurger?: MouseEventHandler<HTMLAnchorElement>;
 }
 
-const WorkMenuItems: FC<PopMenuProps> = ({ closeBurger }) => {
+const WorkMenu: FC<PopMenuProps> = ({ closeBurger }) => {
   return (
-    <Menu.Item>
-      <Menu
-        as="div"
-        className="relative inline-block w-full text-center text-white "
-      >
-        <div className="">
-          <Menu.Button className="inline-flex h-full w-full justify-center focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 md:max-w-xl ">
-            {({ open }) => (
-              <span
-                className={`${
-                  open ? "bg-indigo-800 text-yellow-500" : "text-white"
-                } w-full rounded-t-md p-2`}
-              >
-                Work
-              </span>
-            )}
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className=" grid w-full overflow-hidden rounded-b-md bg-indigo-700 focus:outline-none md:absolute ">
-            <div>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link
-                    href={"/work/pick"}
-                    onClick={closeBurger}
-                    className={`${
-                      active ? "bg-indigo-800 text-yellow-500" : "text-white"
-                    } block w-full  p-2`}
-                  >
-                    pick
-                  </Link>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </Menu.Item>
-  );
-};
-
-const CallsMenuItems = ({ closeBurger: closePop }: PopMenuProps) => {
-  return (
-    <Menu.Item>
-      <Menu
-        as="div"
-        className="relative inline-block w-full text-center text-white "
-      >
+    <Menu
+      as="div"
+      className="relative inline-block w-full text-center text-white "
+    >
+      <div className="">
         <Menu.Button className="inline-flex h-full w-full justify-center focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 md:max-w-xl ">
           {({ open }) => (
             <span
@@ -205,45 +151,93 @@ const CallsMenuItems = ({ closeBurger: closePop }: PopMenuProps) => {
                 open ? "bg-indigo-800 text-yellow-500" : "text-white"
               } w-full rounded-t-md p-2`}
             >
-              Calls
+              Work
             </span>
           )}
         </Menu.Button>
-
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
         <Menu.Items className=" grid w-full overflow-hidden rounded-b-md bg-indigo-700 focus:outline-none md:absolute ">
           <div>
             <Menu.Item>
               {({ active }) => (
                 <Link
-                  href={"/call"}
-                  onClick={closePop}
+                  href={"/work/pick"}
+                  onClick={closeBurger}
                   className={`${
                     active ? "bg-indigo-800 text-yellow-500" : "text-white"
                   } block w-full  p-2`}
                 >
-                  status
-                </Link>
-              )}
-            </Menu.Item>
-          </div>
-
-          <div>
-            <Menu.Item>
-              {({ active }) => (
-                <Link
-                  href={"/call/create"}
-                  onClick={closePop}
-                  className={`${
-                    active ? "bg-indigo-800 text-yellow-500" : "text-white"
-                  }  block w-full p-2`}
-                >
-                  create
+                  pick
                 </Link>
               )}
             </Menu.Item>
           </div>
         </Menu.Items>
-      </Menu>
-    </Menu.Item>
+      </Transition>
+    </Menu>
+  );
+};
+
+const CallsMenu = ({ closeBurger: closePop }: PopMenuProps) => {
+  return (
+    <Menu
+      as="div"
+      className="relative inline-block w-full text-center text-white "
+    >
+      <Menu.Button className="inline-flex h-full w-full justify-center focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 md:max-w-xl ">
+        {({ open }) => (
+          <span
+            className={`${
+              open ? "bg-indigo-800 text-yellow-500" : "text-white"
+            } w-full rounded-t-md p-2`}
+          >
+            Calls
+          </span>
+        )}
+      </Menu.Button>
+
+      <Menu.Items className=" grid w-full overflow-hidden rounded-b-md bg-indigo-700 focus:outline-none md:absolute ">
+        <div>
+          <Menu.Item>
+            {({ active }) => (
+              <Link
+                href={"/call"}
+                onClick={closePop}
+                className={`${
+                  active ? "bg-indigo-800 text-yellow-500" : "text-white"
+                } block w-full  p-2`}
+              >
+                status
+              </Link>
+            )}
+          </Menu.Item>
+        </div>
+
+        <div>
+          <Menu.Item>
+            {({ active }) => (
+              <Link
+                href={"/call/create"}
+                onClick={closePop}
+                className={`${
+                  active ? "bg-indigo-800 text-yellow-500" : "text-white"
+                }  block w-full p-2`}
+              >
+                create
+              </Link>
+            )}
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </Menu>
   );
 };
